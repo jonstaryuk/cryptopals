f2612ba51f222a20e266d4dbfd6176800452735c
Jon Staryuk <jstaryuk@gmail.com>
2016-12-18 11:47:32 -0800

    Fix PKCS#7 padding again, add unpad and tests

--- set2.py
+++ set2.py
@@ -1,8 +1,23 @@
-def pkcs7_pad(data, size):
-    if type(data) == str:
-        data = data.encode()
+def pkcs7_pad(data, block_size=16):
     data = bytearray(data)
-    pad = size - len(data)
+
+    if len(data) < block_size:
+        pad = block_size - len(data)
+    else:
+        pad = len(data) % block_size
+        if pad == 0:
+            pad = block_size
+
     for i in range(pad):
         data.append(pad)
-    return data
+
+    return bytes(data)
+
+
+def pkcs7_unpad(data):
+    data = bytearray(data)
+
+    pad = data[-1]
+    assert pad > 0
+
+    return bytes(data[:-pad])
--- set2_test.py
+++ set2_test.py
@@ -5,8 +5,14 @@ import unittest
 class TestSet2(unittest.TestCase):
 
     def test_pkcs7_pad(self):
-        self.assertEqual(set2.pkcs7_pad(b"YELLOW SUBMARINE", 20), b"YELLOW SUBMARINE\x04\x04\x04\x04")
-        self.assertEqual(set2.pkcs7_pad(b"YELLOW SUBMARINES", 20), b"YELLOW SUBMARINES\x03\x03\x03")
+        self.assertEqual(set2.pkcs7_pad(b"YELLOW SUBMARINE", block_size=20), b"YELLOW SUBMARINE\x04\x04\x04\x04")
+        self.assertEqual(set2.pkcs7_pad(b"YELLOW SUBMARINES", block_size=20), b"YELLOW SUBMARINES\x03\x03\x03")
+        self.assertEqual(set2.pkcs7_pad(b"hello", block_size=5), b"hello\x05\x05\x05\x05\x05")
+
+    def test_pkcs7_unpad(self):
+        self.assertEqual(set2.pkcs7_unpad(b"YELLOW SUBMARINE\x04\x04\x04\x04"), b"YELLOW SUBMARINE")
+        self.assertEqual(set2.pkcs7_unpad(b"YELLOW SUBMARINES\x03\x03\x03"), b"YELLOW SUBMARINES")
+        self.assertEqual(set2.pkcs7_unpad(b"hello\x05\x05\x05\x05\x05"), b"hello")
 
 
 if __name__ == '__main__':
